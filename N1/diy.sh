c1c1b94cf478253b5bd13008e760a57a2f8c96d5
xiaxiaa77 <126748286+xiaxiaa77@users.noreply.github.com>
2025-04-15 23:25:01 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -4,6 +4,10 @@
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 
+# 加入OpenClash核心
+#chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
+#$GITHUB_WORKSPACE/N1/preset-clash-core.sh
+
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
